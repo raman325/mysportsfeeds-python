6fb9e8817cbaf2a8a72d01365d21a75586a1018a
brad-barkhouse <brad.barkhouse@gmail.com>
2020-01-23 20:47:54 -0400

    Increased to v2.1.0 for upload to PyPI.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='ohmysportsfeedspy',
     packages=['ohmysportsfeedspy'],
-    version='2.0.0',
+    version='2.1.0',
     author = ['Brad Barkhouse', 'MySportsFeeds'],
     author_email='brad.barkhouse@mysportsfeeds.com',
     url='https://github.com/MySportsFeeds/mysportsfeeds-python',
     license='MIT',
-    description='A Python wrapper for the MySportsFeeds sports data API'
+    description='A Python wrapper for the MySportsFeeds Sports Data API'
 )
